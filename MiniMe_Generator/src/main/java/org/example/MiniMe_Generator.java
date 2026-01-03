b54eca9bf5f7fa328efbddddd3f15348b0b0bd7d
pai <annika@lenz-online.org>
2026-01-03 14:35:12 +0100

    Design

---
+++ MiniMe_Generator/src/main/java/org/example/MiniMe_Generator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MiniMe_Generator {
+    private JPanel panel1;
+    private JTextField textField_name;
+    private JRadioButton radioButton_gender;
+    private JRadioButton weiblichRadioButton;
+    private JRadioButton diversRadioButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JRadioButton jaRadioButton;
+    private JButton speichernButton;
+    private JRadioButton neinRadioButton;
+    private JTextArea textArea1;
+
+
+}
